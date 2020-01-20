30a68143174547c54ffb3d8261b4fe351712e47f
unknown <robin.luongo@gmail.com>
2020-01-20 01:42:45 -0500

    final

--- src/components/AppComponent.vue
+++ src/components/AppComponent.vue
@@ -1,50 +1,64 @@
 <template>
     <div>
         <div class="hero-bar">
-            <h1 class="title">
+            <h1 class="title yellow">
                 Star Wars Characters
             </h1>
         </div>
-        <div class="card-container container">
-            <CharacterCard 
-                v-for="person in people" 
-                v-bind:key="person.url"
-                v-bind:person="person">
-            </CharacterCard>
+        <div class="container">
+            <div class="card-container">
+                <CharacterCard 
+                    v-for="person in people" 
+                    v-bind:key="person.url"
+                    v-bind:person="person">
+                </CharacterCard>
+            </div>
+            <Paginate
+                :page-count="pages"
+                :click-handler="pageChange"
+                :prev-text="'Prev'"
+                :next-text="'Next'"
+                :container-class="'pagi-container yellow'">
+            </Paginate>
         </div>
     </div>
 </template>
 <script>
 import axios from 'axios'
+import Paginate from 'vuejs-paginate'
 import CharacterCard from './ChracterCard.vue'
-
-const query = () => {
-    
-}
 
    export default {
        data() {
            return {
-               'people': []
+               'people': [],
+               'pages': 0,
+               'currentPage': 1
            }
        },
 
        created: function() { this.query() },
 
        components: {
-           CharacterCard
+           CharacterCard,
+           Paginate
        },
 
         methods: {
             query: function () {
                 const baseUrl = 'https://swapi.co/api/people/'
-                axios.get(baseUrl)
+                const finalUrl = baseUrl + `?page=${this.currentPage}`
+                axios.get(finalUrl)
                     .then(res => res.data)
                     .then(data => {
-                        console.log(data);
                         this.people = data.results
+                        this.pages = Math.ceil(data.count / 10)
                     })
                     .catch(err => console.error(err))
+            },
+            pageChange: function (page) {
+                this.currentPage = page;
+                this.query();
             }
         }
    }
@@ -59,15 +73,38 @@ body, html {
     margin: 0px;
     background-color: #1c1e22;
 }
+.yellow {
+    color: #ffe300; 
+}
 .container {
     margin: auto;
     width: 1300px;
+    padding: 0px 15px 100px 15px;
 }
 .title {
     font-size: 42px;
-    color: #ffe300;
     margin: 0px;
     padding: 75px 0px 70px 0px;
     text-align: center;
+}
+.pagi-container {
+    list-style: none;
+    display: flex;
+    float: right;
+    font-size: 18px;
+}
+.pagi-container > li {
+    padding: 0px 2px;
+}
+.pagi-container > li.active {
+    background-color: #757575;
+}
+.md-theme-default a:not(.md-button) {
+    color: inherit;
+}
+@media(max-width:1300px) {
+    .container {
+        width: 100%;
+    }
 }
 </style>
